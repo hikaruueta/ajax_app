f3effebeb13464a01ca5f6b1ddfc0924091c965d
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-03-22 10:33:23 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
-
-  
+ 
   def create
-    Post.create(content: params[:content])
-  end  
-
-end
-
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
+ end
